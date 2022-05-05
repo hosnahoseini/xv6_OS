abdd86e60bf0c5aac8831cd9df30aa4ffbe969f8
Samin Mahdipour <79986497+Precioux@users.noreply.github.com>
2022-05-05 12:43:09 +0430

    Update proc.h
    stacktop (pointer to top of stack ) is added to proc struct

--- proc.h
+++ proc.h
@@ -50,6 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int ctime;                   // Time when the process created
+  int stackTop;                //points to top of stack
 };
 
 // Process memory is laid out contiguously, low addresses first:
