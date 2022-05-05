7b8761b69f84d59e19210dedea9780c03806153f
Samin Mahdipour <79986497+Precioux@users.noreply.github.com>
2022-05-05 14:08:38 +0430

    Update threads.c
    using right function and comments to understand better
    NOT RUNNABLE YET! //no thread_create function //no System call is created

--- threads.c
+++ threads.c
@@ -3,21 +3,23 @@
 #include "stat.h"
 #include "user.h"
 int x = 0;
+int stack[4096] __attribute__ ((aligned (4096)));
 
 int main(int argc, char *argv[]){
-    int tid = fork();
+    //pass stack to thread create in order to allocate memory
+    int tid = thread_create(stack);
     if(tid < 0){
         printf(2, "error\n");
     }else if(tid == 0){
-        //child
+        //child is born
         for(;;){
-            x++;
+            x++;  //increase x( as much as number of children )
             sleep(100);
         }
     } else{
         //parent
         for(;;){
-            printf(1, "x = %d\n", x);
+            printf(1, "x = %d\n", x); //parent prints value of x (shared memory with children)
             sleep(100);
         }
     }
