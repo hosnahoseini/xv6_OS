ac010d73ad55f2ae3335f84febf448ab1d2e7bb9
hosnahoseini <hosna.hoseini@gmail.com>
2022-05-11 12:12:29 -0700

    [feat] add thread creator

--- thread_creator.c
+++ thread_creator.c
@@ -5,25 +5,24 @@
 #define PAGESIZE 4096
 
 int thread_creator(void (*fn) (void *), void *arg, int status){
-    //alocating 2* pagesize for fptr in heap
     void *fptr = malloc(2 * PAGESIZE);
     void *stack;
     if(fptr == 0)
         return -1;
-    //check if it is page aligned
+
     int mod = (uint) fptr % PAGESIZE;
 
-    if(mod == 0) //if it is page aligned
+    if(mod == 0)
         stack = fptr;
-    else //if it's not
-        stack = fptr + (PAGESIZE - mod);  //move fptr to the right place which is a multiple of pagesize(^_^)
-    //stack must pass to thread create 
-    int thread_id = thread_create((void *)stack);
-    //checking thred_id
-    if(thread_id < 0)   
+    else
+        stack = fptr + (PAGESIZE - mod);
+    
+    int thread_id = thread_create((void *)stack, status);
+
+    if(thread_id < 0)
         printf(1, "thread create failed\n");
     else if (thread_id == 0){
-        //child is made 
+        //child
         (fn)(arg); //call the function passed to thread_create
         free(stack); //free space when function is finished
         exit();
