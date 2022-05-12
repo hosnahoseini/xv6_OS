e3eaedaad7e94c90685528809ca7ea2dd03abc25
hosnahoseini <hosna.hoseini@gmail.com>
2022-05-12 07:43:22 -0700

    [fix] threadTest3

--- threadsTest3.c
+++ threadsTest3.c
@@ -1,29 +1,30 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+int base, limit;
 
 void function(void *args){
     base ++;
     if(base >= limit){
         exit();
     }else{
+    int argument = 0x0F01;
         int thread_id = thread_creator(&function, (void *)&argument);
         int result = thread_join(thread_id);
-        printf("Base = %d, Limit = %d\n", base, limit);
+        printf(1, "Base = %d, Limit = %d\n", base, limit);
 
         if(result == -1)
-            printf("[ID] %d => [Failed] -1\n", thread_id);
+            printf(1, "[ID] %d => [Failed] -1\n", thread_id);
         else
-            printf("[ID] %d => [Success] 0\n", thread_id);
+            printf(1, "[ID] %d => [Success] 0\n", thread_id);
     }
 
 }
 
-int base, limit;
 int main(void){
     int argument = 0x0F01;
-    int thread_id = thread_creator(&childPrint, (void *)&argument);
-    printf("Done! \nBase = %d, Limit = %d", base, limit);
+    thread_creator(&function, (void *)&argument);
+    printf(1, "Done! \nBase = %d, Limit = %d", base, limit);
 
     exit();
 }
