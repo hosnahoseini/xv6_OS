711058f7afeac974b5cc199fe5defea03071c1ea
Precioux <uni.mahdipour@gmail.com>
2022-07-01 07:38:11 -0700

    ML and RR completed

--- multiLevelTest.c
+++ multiLevelTest.c
@@ -2,11 +2,36 @@
 #include "stat.h"
 #include "user.h"
 
-#define N  60
+#define N  30
+
+void callPriority(int priority[]){
+   for(int n=0;n<30;n++){
+            if(n < 5){
+                priority[n]=6;
+            }
+            if(n >= 5 && n < 10){
+                priority[n]=5;
+            }
+            if(n >= 10 && n < 15){
+                priority[n]=4;
+            }
+            if(n >= 15 && n < 20){
+                priority[n]=3;
+            }
+            if(n >= 20 && n < 25){
+                priority[n]=2;
+            }
+            if(n >= 25){
+                priority[n]=1;
+            }
+   }
+}
 
 int main(void){
   int n, pid;
-  int size[] = {60, 10, 10, 10, 10, 10, 10};
+  int size[] = {30, 5, 5, 5, 5, 5, 5};
+  int priority[N];
+  callPriority(priority);
   changePolicy(2);
   printf(1, "Multi Level Test\n");
 
@@ -15,25 +40,25 @@ int main(void){
         if(pid < 0)
             break;
         if(pid == 0){
-            if(n < 10){
+            if(n < 5){
                 setPriority(1, pid);
             }
-            if(n >= 10 && n < 20){
+            if(n >= 5 && n < 10){
                 setPriority(2, pid);
             }
-            if(n >= 20 && n < 30){
+            if(n >= 10 && n < 15){
                 setPriority(3, pid);
             }
-            if(n >= 30 && n < 40){
+            if(n >= 15 && n < 20){
                 setPriority(4, pid);
             }
-            if(n >= 40 && n < 50){
+            if(n >= 20 && n < 25){
                 setPriority(5, pid);
             }
-            if(n >= 50){
+            if(n >= 25){
                 setPriority(6, pid);
             }
-            for(int i=0 ; i<20 ; i++){
+            for(int i=0 ; i<10 ; i++){
                 printf(1, "/%d/ : /%d/ \n", pid, i+1);
             }
             sleep(300);
@@ -46,20 +71,20 @@ int main(void){
 
 
     for (int i = 0; i <30 ; ++i) {
-        int pid = wait();
-        int turnAround = getProcStatus(2, pid) - getProcStatus(1, pid);
+        int pid = wait(); 
         int waitingTime = getProcStatus(4, pid);
         int sleeping = getProcStatus(5, pid);
         int cpuBurst = getProcStatus(3, pid);
+        int turnAround = waitingTime+sleeping+cpuBurst;
         totalTurnaround[0] += turnAround;
         totalWaiting[0] += waitingTime;
         totalBurst[0] += cpuBurst;
 
-        totalTurnaround[getPriority(pid)] += turnAround;
-        totalWaiting[getPriority(pid)] += waitingTime;
-        totalBurst[getPriority(pid)] += cpuBurst;
-        printf(1, "PID: %d | Turnaround Time: %d | Waiting Time: %d"
-                  " | CPU Burst Time: %d | Sleeping Time: %d\n", pid, turnAround, waitingTime, cpuBurst, sleeping);
+        totalTurnaround[priority[i]] += turnAround;
+        totalWaiting[priority[i]] += waitingTime;
+        totalBurst[priority[i]] += cpuBurst;
+        printf(1, "%d - PID: %d | Turnaround Time: %d | Waiting Time: %d"
+                  " | CBT: %d | Sleeping Time: %d | Priority: %d\n",i+1, pid, turnAround, waitingTime, cpuBurst, sleeping,priority[i]);
     }
 
     for (int i = 0; i <7 ; ++i) {
@@ -72,6 +97,5 @@ int main(void){
         printf(1, "priority: %d average turnaround: %d  waiting: %d  burst: %d \n",
                i, totalTurnaround[i], totalWaiting[i], totalBurst[i]);
     }
-
     exit();
 }
--- roundRobinTest.c
+++ roundRobinTest.c
@@ -8,10 +8,10 @@ int main() {
     for (int i = 0; i <10 ; i++) {
         int pid = fork();
         if ( pid == 0) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 1000; i++) {
                 printf(1, "/%d/ : /%d/\n", getpid(), i);
             }
-            sleep(300);
+            sleep(800);
             exit();
         }
         else children= children +1;
@@ -20,8 +20,7 @@ int main() {
     int sumTurnaround = 0;
     int sumWaiting = 0;
     int sumBurst = 0;
-    
-    
+
 
     for (int i = 0; i <10 ; i++) {
         int pid = wait();
@@ -32,23 +31,21 @@ int main() {
         sumTurnaround += turnAround;
         sumWaiting += waitingTime;
         sumBurst += cpuBurst;
-        printf(1, "pid: %d, ", getpid());
-        printf(1, "creation time = %d, ", getProcStatus(1, pid));
+        printf(1, "pid: %d, ", pid);
         printf(1, "turnaround time = %d, ", turnAround);
         printf(1, "waiting time = %d, ", waitingTime);
         printf(1, "sleeping time = %d, ", sleeping);
         printf(1, "cpu burst = %d, ", cpuBurst);
         printf(1,"\n");
-
+        
 
     }
    
-    double avgTurnaround = (double) sumTurnaround / 10;
-    double avgWaiting = (double) sumWaiting / 10;
-    double avgCBT = (double) sumBurst / 10;
-    printf(1, "\naverage turnaround time: %lf\n",avgTurnaround);
-    printf(1, "average waiting time: %lf\n",avgWaiting);
-    printf(1, "average cpu burst time: %lf\n",avgCBT);
-    printf(1,"Number of children: %d\n",children);
+    int avgTurnaround =  sumTurnaround / 10;
+    int avgWaiting =  sumWaiting / 10;
+    int avgCBT = sumBurst / 10;
+    printf(1, "\naverage turnaround time: %d\n",avgTurnaround);
+    printf(1, "average waiting time: %d\n",avgWaiting);
+    printf(1, "average cpu burst time: %d\n",avgCBT);
     exit();
 }
